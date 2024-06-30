677176a573ac3744bfc43931406f3655e50f3a02
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-07-01 00:34:12 +0530

    mongo error fixing

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 dotenv.config()
-const MONGODB_URI = process.env.mongodb_url;
+const MONGODB_URI = "mongodb+srv://afsalkpmanu31:afsalkp123@cluster0.yzytvxu.mongodb.net/StudentManagement?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(MONGODB_URI)
 .then(()=>{
     console.log("Mongoose connected successfully");
